b195b25d1c42065ffb66a71d26938b72da9c03b2
agdavid <antonio.g.david@gmail.com>
2016-10-28 15:56:41 -0400

    add image validation tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -17,7 +17,7 @@ class ProductTest < ActiveSupport::TestCase
       product = Product.new(:title => "My Book Title",
                             :description => "yyy",
                             :image_url => "zzz.jpg"
-        )
+                            )
       product.price = -1
       assert product.invalid?
       assert_equal "must be greater than or equal to 0.01",
@@ -30,5 +30,26 @@ class ProductTest < ActiveSupport::TestCase
 
       product.price = 1
       assert product.valid?
+  end
+
+  def new_product(image_url)
+      Product.new(:title => "My Book Title",
+                  :description => "yyy",
+                  :price => 1,
+                  :image_url => image_url
+                  )
+  end
+
+  test "product image_url must have valid extentsion" do 
+      ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+      bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+      ok.each do |name|
+          assert new_product(name).valid?, "#{name} shouldn't be invalid"
+      end
+
+      bad.each do |name|
+          assert new_product(name).invalid?, "#{name} shouldn't be valid"
+      end
   end
 end
