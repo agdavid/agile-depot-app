235fb24cfe7b0d69d4a59b6b4f3d7841ffe0dd02
agdavid <antonio.g.david@gmail.com>
2016-10-31 15:33:42 -0400

    create add_to_cart method for quantity of LineItem

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,16 @@
 class Cart < ApplicationRecord
     has_many :line_items, :dependent => :destroy
+
+    def add_product(product_id)
+        current_item = line_items.where(:product_id => product_id).first
+        if current_item
+            # if searching for a line_item by the product_id results in a match
+            # bump up the quantity
+            current_item.quantity += 1
+        else
+            # build a new line_item
+            current_item = line_items.build(:product_id => product_id)
+        end
+        current_item
+    end
 end
