d079041cb58941ed426572ac221f5b143340492c
agdavid <antonio.g.david@gmail.com>
2016-10-31 15:03:11 -0400

    add ORM

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
     default_scope { order('title') }#set default order to alphabetical by title
+    has_many :line_items
+    before_destroy :ensure_not_referenced_by_any_line_item
 
     validates :title, :description, :image_url, :presence => true
     validates :price, :numericality => { :greater_than_or_equal_to => 0.01 }
@@ -9,4 +11,14 @@ class Product < ApplicationRecord
         :multiline => true,
         :message => 'must a URL for GIF, JPG or PNG image.'
     }
+
+    def ensure_not_referenced_by_any_line_item
+        if line_items.count.zero?
+            # okay to delete product, if no corresponding line_items
+            return true
+        else
+            errors.add(:base, 'Line Items present')
+            return false
+        end
+    end
 end
